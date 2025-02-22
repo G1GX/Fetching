89f9681526a40814ca75e25adfc034674a341744
Kanyanat <getgigxdotnet@gmail.com>
2025-02-23 00:29:06 +0700

    Update Loader.lua

--- Master/Loader.lua
+++ Master/Loader.lua
@@ -5,7 +5,7 @@ if _G.Languages == "Thai" or getgenv().Languages == "Thai" then
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/BuildeABoat.ex")
 	elseif _id == 16732694052 then --Fisch
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Fisch.TH")
-	elseif _id == 16981421605 then -- Dead Rails
+	elseif _id == 70876832253163 then -- Dead Rails
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Dead_Rails")
 	end
 end
