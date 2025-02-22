10c28131a82f6a671e0cf1bf2f95e95a2a6d451f
Kanyanat <getgigxdotnet@gmail.com>
2025-02-23 00:15:45 +0700

    Update Loader.lua

--- Master/Loader.lua
+++ Master/Loader.lua
@@ -1,11 +1,11 @@
 _id = game.PlaceId
 loadstr = function(raw)loadstring(game:HttpGet(raw))()end
-if _G.Languages == "Thai" then
+if _G.Languages == "Thai" or getgenv().Languages == "Thai" then
 	if _id == 537413528 then --Build
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/BuildeABoat.ex")
 	elseif _id == 16732694052 then --Fisch
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Fisch.TH")
-	elseif _id == 70876832253163 then -- Dead Rails
+	elseif _id == 16981421605 then -- Dead Rails
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Dead_Rails")
 	end
 end
