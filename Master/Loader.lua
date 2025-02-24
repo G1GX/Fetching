647a31d01e4603cc5986bb36b77bf0502cd41d0c
Kanyanat <getgigxdotnet@gmail.com>
2025-02-25 00:27:01 +0700

    Update Loader.lua

--- Master/Loader.lua
+++ Master/Loader.lua
@@ -1,11 +1,19 @@
-_id = game.PlaceId
 loadstr = function(raw)loadstring(game:HttpGet(raw))()end
-if _G.Languages == "Thai" or getgenv().Languages == "Thai" then
-	if _id == 537413528 then --Build
+if _G.Thai or _G.Languages == "Thai" or getgenv().Languages == "Thai" then
+	if game.PlaceId == 537413528 then --Build
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/BuildeABoat.ex")
-	elseif _id == 16732694052 then --Fisch
+	elseif game.PlaceId == 16732694052 then --Fisch
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Fisch.TH")
-	elseif _id == 70876832253163 then -- Dead Rails
+	elseif game.PlaceId == 70876832253163 then -- Dead Rails
+		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Dead_Rails")
+	end
+end
+if _G.English then
+	if game.PlaceId == 537413528 then --Build
+		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/BuildeABoat.ex")
+	elseif game.PlaceId == 16732694052 then --Fisch
+		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Fisch.TH")
+	elseif game.PlaceId == 70876832253163 then -- Dead Rails
 		loadstr("https://raw.githubusercontent.com/G1GX/Fetching/refs/heads/main/Master/Dead_Rails")
 	end
 end
